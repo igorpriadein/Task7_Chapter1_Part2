cfd59abae5f9a2e9b8e328d99c97aa1e5aa49a90
igorpriadein <iga2456@gmail.com>
2020-02-28 18:24:28 +0600

    Task7 Part2

---
+++ task7.py
@@ -0,0 +1,15 @@
+X = int(input("Enter your integer "))
+if X > 0:
+    print("1")
+elif X < 0:
+    print("-1")
+else:
+    print("0")
+
+
+
+
+
+# For the given inor it.teger X print 1 if it's positive, -1 if it's negative, or 0 if it's equal
+# # to zero.
+# # Try to use the cascade if-elif-else f
